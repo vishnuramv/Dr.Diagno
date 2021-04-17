964a23f293b59c1fe5f03b76eaee0032f0af629f
GanapathyPT <18ganapathy04@gmail.com>
2021-04-17 18:28:03 +0530

    resolved error

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,59 +1,58 @@
 // import { Doughnut } from '@reactchartjs/react-chart.js'
-import { useEffect, useState } from 'react'
-import { Doughnut } from 'react-chartjs-2'
-import { getTopDisease } from '../helper'
+import { useEffect, useState } from "react";
+import { Pie } from "react-chartjs-2";
+import { getTopDisease } from "../helper";
 
 const Chart = ({ predictedDisease }) => {
-    const [topDisease, setTopDisease] = useState({})
+  const [topDisease, setTopDisease] = useState({});
 
-    useEffect(() => {
-        const disease = getTopDisease(predictedDisease)
-        setTopDisease(disease)
-    }, [predictedDisease])
-    const data = {
-        labels: Object.keys(topDisease),
-        datasets: [
-            {
-                label: '# of Diseases',
-                data: Object.values(topDisease),
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.5)',
-                    'rgba(54, 162, 235, 0.5)',
-                    'rgba(255, 206, 86, 0.5)',
-                    'rgba(153, 102, 255, 0.5)',
-                    'rgba(255, 159, 64, 0.5)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                ],
-                borderWidth: 1,
-            },
+  useEffect(() => {
+    const disease = getTopDisease(predictedDisease);
+    setTopDisease(disease);
+  }, [predictedDisease]);
+  const data = {
+    labels: Object.keys(topDisease),
+    datasets: [
+      {
+        label: "# of Diseases",
+        data: Object.values(topDisease),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.5)",
+          "rgba(54, 162, 235, 0.5)",
+          "rgba(255, 206, 86, 0.5)",
+          "rgba(153, 102, 255, 0.5)",
+          "rgba(255, 159, 64, 0.5)",
         ],
-    }
-    const options = {
-        legend: {
-            display: true,
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  const options = {
+    legend: {
+      display: true,
+      position: window.innerWidth > 420 ? "right" : "top",
+    },
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItem, data) {
+          const label = data.labels[tooltipItem.index];
+          const value =
+            data.datasets[tooltipItem.datasetIndex]?.data[tooltipItem.index];
+          return `${label}: ${value}%`;
         },
-        tooltips: {
-            callbacks: {
-                label: function (tooltipItem, data) {
-                    const label = data.labels[tooltipItem.index];
-                    const value =
-                        data.datasets[tooltipItem.datasetIndex]?.data[
-                        tooltipItem.index
-                        ];
-                    return `${label}: ${value}%`;
-                },
-            },
-        },
-    };
-    return (
-        <Doughnut data={data} options={options} width={500} height={100} />
-    )
-}
+      },
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+  return <Pie data={data} options={options} height={100} width={50} />;
+};
 
-export default Chart
+export default Chart;
--- src/pages/SymptomResult.jsx
+++ src/pages/SymptomResult.jsx
@@ -1,14 +1,14 @@
-import Chart from '../components/Chart'
-import '../styles/SymptomResult.css'
+import Chart from "../components/Chart";
+import "../styles/SymptomResult.css";
 const SymptomResult = ({ location }) => {
-    return (
-        <div className="result">
-            <h1>Predition Results</h1>
-            <div className="result__graphContainer">
-                <Chart predictedDisease={location.state.predictedDisease} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="result">
+      <h1>Predition Results</h1>
+      <div className="result__graphContainer">
+        <Chart predictedDisease={location.state.predictedDisease} />
+      </div>
+    </div>
+  );
+};
 
-export default SymptomResult
+export default SymptomResult;
